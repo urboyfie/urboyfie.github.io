f6b742d7cece3101161739b176af59183f17fb95
urboyfie <110891404+urboyfie@users.noreply.github.com>
2022-08-09 14:43:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function open_card() {
+    alert('hai cantikk')
+    alert('DOORRRR!')
+    alert('Happy birthday to you')
+    alert('maaf cuma bisa bikin ginian wkwkk')
+  document.getElementById('outside').className = 'open-card';
+}
+
+function close_card() {
+    alert('byee')
+  document.getElementById('outside').className = '';
+}
